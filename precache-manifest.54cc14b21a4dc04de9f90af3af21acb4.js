e5860c8ca157096d6262a8ff15827c1d58b967a5
komaljituri <komaljituri88@gmail.com>
2019-11-28 14:12:09 +0530

    Updates

---
+++ precache-manifest.54cc14b21a4dc04de9f90af3af21acb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f060be71f4e27fd43fd6b92346ae8a",
+    "url": "/Greendeck-Task/index.html"
+  },
+  {
+    "revision": "b94bcbbd6a70ebd5357f",
+    "url": "/Greendeck-Task/static/css/main.4a7f8222.chunk.css"
+  },
+  {
+    "revision": "d4175f71449108125678",
+    "url": "/Greendeck-Task/static/js/2.567a93d8.chunk.js"
+  },
+  {
+    "revision": "b94bcbbd6a70ebd5357f",
+    "url": "/Greendeck-Task/static/js/main.647052dd.chunk.js"
+  },
+  {
+    "revision": "aaa79edb8bec9b9517be",
+    "url": "/Greendeck-Task/static/js/runtime-main.7fa04de1.js"
+  }
+]);
